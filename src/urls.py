f72df45fced05a898ffcbf29983ebc486d12f186
landryonana <51939280+landryonana@users.noreply.github.com>
2022-02-01 16:25:56 +0100

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from wkhtmltopdf.views import PDFTemplateView
 import os
 
 
